86bd2bee3662ccd55de67df108825c0c85cebcf0
Swati <swatis4494@gmail.com>
2020-05-04 20:47:32 +0530

    social links added

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <div className="cardContainer">
         <div className="card">
           <div className="card-content">
-          <div>
-              <figure className="image is-48x48">
+          <div className="card-image">
+              <figure className="image is-54x54">
                 <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
               </figure>
             </div>
-            <div className="media">
-              <div className="media-content">
-                <p className="title is-4">John Smith</p>
-                <p className="subtitle is-6">@johnsmith</p>
+            <div className="card-name">
+              <div>
+                <p className="title is-3">Swati Sharma</p>
+                <p className="subtitle is-4">Sr. Software Developer</p>
               </div>
             </div>
 
@@ -26,6 +26,12 @@ function App() {
               <br />
               <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
             </div>
+          </div>
+          <div className="card-social-links">
+              <a href="#"><i class="fab fa-facebook"></i></a>
+              <a href="#"><i class="fab fa-linkedin"></i></a>
+              <a href="#"><i class="fab fa-instagram-square"></i></a>
+              <a href="#"><i class="fab fa-github-square"></i></a>
           </div>
           </div>
         </div>
